0aac36fe6caaa954ce66a968b83eca478edba94d
300943385 <300943385@PR-E216-41>
2019-07-08 14:22:24 -0400

    add function about calculator button

---
+++ week09.1/CalculateForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace week09._1
+{
+    public partial class CalculateForm : Form
+    {
+        public CalculateForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateButtonTableLayoutPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void CalculateForm1_Click(object sender, EventArgs e)
+        {
+            var TheButton = sender as Button;
+
+            int buttonValue;
+            bool resultCondition = int.TryParse(TheButton.Text, out buttonValue);
+
+            if (resultCondition)
+            {
+                ResultLabel.Text += TheButton.Text;
+            }
+            else
+            {
+                ResultLabel.Text = "Not a number";
+            }
+
+
+            ResultLabel.Text += TheButton.Text;
+        }
+    }
+}
--- week09.1/Program.cs
+++ week09.1/Program.cs
@@ -16,7 +16,7 @@ namespace week09._1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new CalculateForm1());
+            Application.Run(new CalculateForm());
         }
     }
 }
